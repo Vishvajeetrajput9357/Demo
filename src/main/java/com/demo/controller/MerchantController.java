39b2c8273052671f8cbc67752b8788100a70b2af
Vishvajeet_rajput <jeet769826@gmail.com>
2024-11-16 13:46:09 +0530

    first commit

--- src/main/java/com/demo/controller/MerchantController.java
+++ src/main/java/com/demo/controller/MerchantController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,18 +17,34 @@ import com.demo.request.LoginRequest;
 public class MerchantController {
 
 	@PostMapping("/login")
-	public Map<String, Object> loginMerchant(@RequestBody @Valid LoginRequest loginRequest) {
-
+	public Map<String, Object> loginMerchant(@RequestBody @Valid LoginRequest loginRequest,BindingResult result) {
 		Map<String, Object> map = new HashMap<String, Object>();
+		try {
 
-		map.put("bussinessName", "HP Gas");
-		map.put("status", "Success");
-		map.put("date", "12-11-2024 22:39:13");
-		map.put("merchantUserName", "HP Gas");
-		map.put("loggedIn", true);
-		map.put("firstName", "Vishvajeet");
-		map.put("code", "0x0200");
-		map.put("description", "Login Successfully");
+			if (!loginRequest.getPassword().equalsIgnoreCase("Fail")) {
+				map.put("bussinessName", "HP Gas");
+				map.put("status", "Success");
+				map.put("date", "12-11-2024 22:39:13");
+				map.put("merchantUserName", "HP Gas");
+				map.put("loggedIn", true);
+				map.put("firstName", "Vishvajeet");
+				map.put("code", "0x0200");
+				map.put("description", "Login Successfully");
+			} else {
+				map.put("status", "Failure");
+				map.put("loggedIn", false);
+				map.put("code", "0x0202");
+				map.put("description", "Invalid user email or password");
+			}
+
+		} catch (Exception e) {
+			System.out.println(result.getFieldError().getDefaultMessage());
+			e.printStackTrace();
+			map.put("status", "Failure");
+			map.put("loggedIn", false);
+			map.put("code", "0x0202");
+			map.put("description", "Invalid user email or password");
+		}
 		return map;
 	}
 
--- src/main/java/com/demo/request/LoginRequest.java
+++ src/main/java/com/demo/request/LoginRequest.java
@@ -1,27 +1,31 @@
 package com.demo.request;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 public class LoginRequest {
 
-	@NotNull
+	@NotBlank
 	private String email;
 	
-	@NotNull
+	@NotBlank
 	private String password;
 
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	
 
 }
